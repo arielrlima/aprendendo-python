727a5f92470aff023c655146199b83f9de9b09d8
Ariel Lima <arielramosdelima@gmail.com>
2022-09-13 22:19:00 -0300

    Mais testes

--- file1.py
+++ file1.py
@@ -4,3 +4,8 @@ if dinheiro>10000:
   imps = dinheiro * 0.1
   multa = dinheiro * 0.15
   print( "Valor do Imposto:", imps, "e da multa:", multa)
+
+  x, y, z = "Orange", "Banana", "Cherry"
+  print(x)
+  print(y)
+  print(z)
